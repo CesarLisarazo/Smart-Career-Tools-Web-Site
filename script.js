324dcf642b042b1ad9d9d9386fa3e1a89a767224
Cesar Lisarazo <cesarlisarazo@gmail.com>
2026-02-07 04:26:17 -0300

    hint chasing

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ if (contactLink && textEl) {
 
       setTimeout(() => {
         contactLink.classList.remove("show-hint");
-      }, 2000);
+      }, 2300);
     }
 
     // 4️⃣ Volver a "Contacto" con animación
